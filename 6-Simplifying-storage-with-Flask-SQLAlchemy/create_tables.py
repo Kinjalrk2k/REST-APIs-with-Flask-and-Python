bce4bda586ddc54e65c802b2c49a323c3954d900
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-08-05 11:20:37 +0530

    setting up the project

---
+++ 6-Simplifying-storage-with-Flask-SQLAlchemy/create_tables.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+DB_FILE = "data.db"
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+cursor.execute(create_table)
+
+create_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+cursor.execute(create_table)
+
+connection.commit()
+connection.close()
