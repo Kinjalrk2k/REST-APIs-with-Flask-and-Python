0c1e2389ae35031c99b191935da679c23c778e79
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-08-08 10:24:49 +0530

    blacklisting users implemented

--- 7-Token-refreshing-and-Flask-JWT-Extended/app.py
+++ 7-Token-refreshing-and-Flask-JWT-Extended/app.py
@@ -6,14 +6,18 @@ from flask_jwt_extended import JWTManager
 from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'asupersecretkeythatnobodyknows'
-api = Api(app)
 
+app.secret_key = 'asupersecretkeythatnobodyknows'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+api = Api(app)
 
 jwt = JWTManager(app)
 
@@ -23,6 +27,11 @@ def add_claims_to_jwt(identity):
     if identity == 1:
         return {'is_admin': True}
     return {'is_admin': False}
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blacklist(jwt_headers, jwt_payload):
+    return jwt_payload['sub'] in BLACKLIST
 
 
 @jwt.expired_token_loader
@@ -58,7 +67,7 @@ def token_not_fresh_callback():
 
 
 @jwt.revoked_token_loader
-def revoked_token_callback():
+def revoked_token_callback(jwt_header, jwt_payload):
     return jsonify({
         "description": "The token has been revoked.",
         'error': 'token_revoked'
